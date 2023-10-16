93c6ec6f2ebad3adaabcac0f143fab7792b0f78e
bekirbali <bfbali43@gmail.com>
2023-10-16 09:48:27 +0300

    up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Mermer ve Tezgahlar</h1>
+      <p>
+        1943’ten bugüne doğal taş üretiminde Türkiye’nin öncülerinden olan
+        Efesus Stone’da mutfak, banyo ve diğer yaşam alanlarınız için mükemmel
+        malzeme tercihinizi yapın. Binlerce taş ve 199’dan fazla renkten oluşan
+        kataloglarımızı inceleyin.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
