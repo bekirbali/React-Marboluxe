cf6370a05c1944c0672da8d1bde56624aba7e82a
bekirbali <bfbali43@gmail.com>
2023-10-18 16:51:17 +0300

    flowbite update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex bg-slate-600 text-white p-2 ">
+    <footer className="flex flex-col md:flex-row bg-slate-600 text-white p-2 ">
       <div className="hakkımızda flex-1">
-        <h2 className="font-bold text-2xl">Hakkımızda</h2>
+        <h2 className="font-bold text-xl md:text-2xl">Hakkımızda</h2>
         <p>
           Marboluxe Mermer, 1943’ten bugüne doğal taş üretiminde Türkiye’nin
           öncü firmalarından biridir.
         </p>
       </div>
       <div className="bizeUlaşın flex flex-col gap-2 flex-1 justify-center">
-        <h2 className="font-bold text-2xl">Bize Ulaşın</h2>
+        <h2 className="font-bold text-xl md:text-2xl">Bize Ulaşın</h2>
         <h3 className="font-semibold">Merkez Fabrika</h3>
         <p>
           Ankara Karayolu Üzeri 22.km, 03750 İscehisar / Afyonkarahisar /
