9e4d6583e0588b277ed5b6851a6a38e73677335e
bekirbali <bfbali43@gmail.com>
2023-10-21 17:26:36 +0300

    special collection

--- src/pages/OzelKoleksiyon.jsx
+++ src/pages/OzelKoleksiyon.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { dogalTaslar } from "../utils";
+import { ozelKoleksiyon } from "../utils";
 
 const OzelKoleksiyon = () => {
   return (
     <div className="p-4">
       <h1 className="text-center text-3xl font-bold my-4">Özel Koleksiyon</h1>
       <div className="flex flex-wrap justify-center gap-4">
-        {dogalTaslar[0].map((tas) => {
+        {ozelKoleksiyon[0].map((tas) => {
           return (
-            <div className="flex flex-col items-center justify-center w-[24%]">
+            <div className="flex flex-col items-center justify-center w-[24%] shadow-sm shadow-cyan-700">
               <img src={tas.image} alt="test" className="w-full h-[250px] " />
-              <p className="text-center">{tas.name}</p>
+              <p className="text-center p-4">{tas.name}</p>
             </div>
           );
         })}
