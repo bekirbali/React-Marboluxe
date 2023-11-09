00b4c118c253273ecc20cc6b47d530d51c72c9e5
bekirbali <bfbali43@gmail.com>
2023-11-09 21:20:49 +0300

    new buttons for next and prev page

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  BsFillArrowRightCircleFill,
+  BsFillArrowLeftCircleFill,
+} from "react-icons/bs";
+
+const Buttons = () => {
+  return (
+    <>
+      <BsFillArrowLeftCircleFill />
+      <BsFillArrowRightCircleFill />
+    </>
+  );
+};
+
+export default Buttons;
