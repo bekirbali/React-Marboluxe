ec31d5c566fe94b6bd8eac7259782084a6047a33
bekirbali <bfbali43@gmail.com>
2023-11-12 23:43:40 +0300

    sidebar added to "ocaklar" page

--- src/pages/Ocaklar.jsx
+++ src/pages/Ocaklar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ocaklar } from "../utils";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Breadcrumbs from "../components/Breadcrumbs";
 
 const Ocaklar = () => {
@@ -9,18 +9,53 @@ const Ocaklar = () => {
   return (
     <>
       <Breadcrumbs />
-      <div className="main-holder-ocaklar flex justify-between w-[90%] mx-auto">
-        <div className="side-bar flex-[1]">
-          <div className="tezgahlar">
-            <h3 className="font-bold">Tezgahlar</h3>
 
-            <ul className="ml-2 border-l-2 border-gray-300 pl-2 flex flex-col gap-2">
-              <li>Caesarstone Tezgah</li>
-              <li>Calisco Tezgah</li>
-              <li>Belenco Tezgah</li>
-              <li>Çimstone Tezgah</li>
+      <div className="main-holder-ocaklar flex justify-between w-[90%] mx-auto">
+        <div className="flex-[1]">
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/urunler"
+          >
+            Ürünler
+          </NavLink>
+          <ul className="ml-2 border-l-2 border-gray-300 pl-2">
+            <ul className="cnc">
+              <NavLink to="/urunler/cnc">CNC Ürünler</NavLink>
+              <div className="ml-2 border-l-2 border-gray-300 pl-2 flex flex-col">
+                <NavLink to="/urunler/cnc/duvar">Duvar Dekorasyonları</NavLink>
+                <NavLink to="/urunler/cnc/masalar">Masalar</NavLink>
+                <NavLink to="/urunler/cnc/sehpalar">Sehpalar</NavLink>
+                <NavLink to="/urunler/cnc/tabureler">Tabureler</NavLink>
+              </div>
             </ul>
-          </div>
+            <ul className="waterjet">
+              <NavLink to="/urunler/waterjet">Waterjet</NavLink>
+              <div className="ml-2 border-l-2 border-gray-300 pl-2 flex flex-col">
+                <NavLink to="/urunler/waterjet/madalyon">
+                  Waterjet Madalyon
+                </NavLink>
+                <NavLink to="/urunler/waterjet/mozaik">Waterjet Mozaik</NavLink>
+                <NavLink to="/urunler/waterjet/bordur">Waterjet Bordür</NavLink>
+                <NavLink to="/urunler/waterjet/karo">Waterjet Karo</NavLink>
+              </div>
+            </ul>
+            <ul className="dekoratif-urunler">
+              <NavLink to="/urunler/dekoratif">Dekoratif Ürünler</NavLink>
+              <div className="ml-2 border-l-2 border-gray-300 pl-2 flex flex-col">
+                <NavLink to="/urunler/dekoratif/supurgelik">Süpürgelik</NavLink>
+                <NavLink to="/urunler/dekoratif/kolon">Kolon</NavLink>
+                <NavLink to="/urunler/dekoratif/kurna">Kurna</NavLink>
+                <NavLink to="/urunler/dekoratif/sutun">Sütun</NavLink>
+                <NavLink to="/urunler/dekoratif/korkuluk">Korkuluk</NavLink>
+                <NavLink to="/urunler/dekoratif/sove">Söve</NavLink>
+                <NavLink to="/urunler/dekoratif/dus">Duş Teknesi</NavLink>
+                <NavLink to="/urunler/dekoratif/bahce">Bahçe Süslemesi</NavLink>
+                <NavLink to="/urunler/dekoratif/aksesuar">Aksesuar</NavLink>
+                <NavLink to="/urunler/dekoratif/aplik">Aplik</NavLink>
+                <NavLink to="/urunler/dekoratif/sampuanlik">Şampuanlık</NavLink>
+              </div>
+            </ul>
+          </ul>
         </div>
         <div className="p-4 flex flex-col items-center flex-[4]">
           <h1 className="text-center text-3xl font-bold my-4">Ocaklar</h1>
