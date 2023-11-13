c65561016952c23dfa7481170e0ea139260d9585
bekirbali <bfbali43@gmail.com>
2023-11-14 00:20:07 +0300

    Fix typos and update content in SideBar and DogalTaslarBejMermer components

--- src/pages/dogal-taslar/DogalTaslarOniks.jsx
+++ src/pages/dogal-taslar/DogalTaslarOniks.jsx
@@ -15,7 +15,10 @@ const DogalTaslarOniks = () => {
         </div>
 
         <div className="p-4 flex flex-col items-center flex-[4]">
-          <h1 className="text-center text-3xl font-bold my-4">Oniks</h1>
+          <p className="xl:pl-8 xl:m-4 py-4 pb-8 text-[#777777]">
+            Doğadan aldığı ilham ile hayat bulan Efesus Stone oniks mermer
+            çeşitleri ve fiyatları için özel tasarım koleksiyonumuzla tanışın!
+          </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center gap-4 ">
             {dogalTaslarOniks[0].map((tas, index) => {
               return (
@@ -39,6 +42,134 @@ const DogalTaslarOniks = () => {
                 </div>
               );
             })}
+          </div>
+          <div className="w-full flex flex-col gap-4 mt-4 border-t pt-2">
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Onyx (Oniks) Mermer Çeşitleri
+            </h2>
+            <p className="text-[#777777]">
+              Kalsedon taş grubu ailesinde sayılan Onyx (Oniks) Mermer taşı yarı
+              değerli taş grubu içerisinde yer almaktadır. Oluşum süreci diğer
+              değerli mermer türlerine göre kalsiyum karbonatın yoğun olduğu
+              soğuk su kaynaklarında gerçekleşir. İçerik bakımından daha yoğun
+              madeni tuz içeren suların daha yavaş çökelme işlemi sonucunda
+              oluşum tamamlanır. Çözülme işleminin yavaş gerçekleşmesi ise Oniks
+              Mermerin daha kristalize, saydam ve yoğun bir yapıya bürünmesini
+              sağlar. Saydam ve kristalize bir yapısının olması onu diğer mermer
+              türleri arasında farklı bir noktaya taşır. Bu durum Onyx Mermerin
+              ışığı geçirebilme özelliğine sahip olmasında etkilidir. Diğer
+              doğal taşlar garklı olarak ışığı geçirme özelliğinin yarı değerli
+              taş grubunda yer almasını sağlar. Bu özellik Oniks taşının mermer
+              dışında kullanım alanlarına sahip olmasında en büyük etkendir.
+              Yapı malzemesi dışında dekorasyon, süs eşyası ve takı sektöründe
+              de bu ürüne ilgi büyüktür.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Onyx (Oniks) Mermer Nedir?
+            </h2>
+            <p className="text-[#777777]">
+              Diğer mermer türlerine göre oniks mermer içeriğinde bulundurduğu
+              silisyum mineralleri ile farklı bir özelliğe sahiptir. Saydam ve
+              kristalize bir yapıya sahip olmasından dolayı Onyx Mermer; çok
+              daha değerli bir doğal taştır. Işığı geçirme özelliği onu farklı
+              mimari ve dekorasyon ürünü olarak kullanma imkanı tanır.
+              Belirttiğimiz özelliği nedeniyle oldukça değerli ve yer altından
+              çıkarılması zor olan Oniks mermerleri kullanıldığı alanlara farklı
+              bir estetik ve değer katmaktadır. Doğal taş madenleri bakımından
+              oldukça elverişli bir coğrafyaya sahip olmamız; Onyx mermer
+              üretimimizi olumlu etkilemektedir. Özellikle Afyon ve Adıyaman
+              bölgelerinde oniks mermer ocakları bulunmaktadır. Diğer mermer
+              türlerine göre çıkarılması çok daha zor olana bu özel ürün özel
+              araç ve makineler kullanılarak kullanıma hazır hale
+              getirilmektedir. Çıkarılmasının ve işlenmesinin güçlüğünün yanında
+              tüm işlemleri tamamlanan bir oniks mermer diğer mermerlere göre
+              çok daha özel bir görünüme sahiptir. Kullanıldığı alanlara çok
+              farklı ve zengin bir görünüm kazandıran bu ürün genellikle topluma
+              açık binalarda tercih edilmektedir. Işık geçirgen yapısı sayesinde
+              aydınlık mekanlarda veya özel ışıklandırmayla ürünün içindeki renk
+              cümbüşünü ortaya çıkarabilir ve tüm ilgiyi oniks mermerin üzerine
+              çekebilirsiniz.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Oniks Mermer Nerelerde Kullanılır?
+            </h2>
+            <p className="text-[#777777]">
+              Oniks mermer ocaklarından özenle çıkarılan bloklar istenilen
+              ölçüler için özenle kesilirler. Daha sonra özel tornalarda şekil
+              verme işlemleri gerçekleştirilir. Bu iş için özel makinelerde işin
+              uzmanları tarafından titizlikle hazırlanan oniks mermerler
+              kullanıma hazır hale getirilir. Diğer mermer türlerine göre çok
+              daha az sayıda üretilen bu özel ürün genellikle iç mekan
+              dekorasyonlarında kullanılır. Işığı geçiren yapısı nedeniyle iç
+              mekanlara ayrı bir zarafet katan oniks mermer ürünleri tamamlayıcı
+              tasarım ürünü olarak kullanılır. Otellerin lobilerinde, tiyatro,
+              gösteri alanı, toplantı salonları ve lüks restoranlarda sıklıkla
+              bu ürün tercih edilmektedir. Uygulama alanlarına farklı ışık
+              vererek çok daha güzel ve zengin bir görünüm elde etmek mümkündür.
+              Bu tür yerlerin dışında oniks mermer metal veya ahşap ayakların
+              üzerinden masa veya sehpa ürünü olarak da tercih edilebilir. Yaşam
+              alanlarına farklı bir görsel güzellik katmak adına kullanılan Onyx
+              mermer ayrıca dekoratif ürün olarak da kullanılmaktadır. Özel
+              olarak hazırlanan heykeller, tabak, çanak, takı aksesuarı ve el
+              işi küçük oyma objelerde bu ürünün kullanıldığını görmeniz mümkün.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Onyx Mermer Çeşitleri
+            </h2>
+            <p className="text-[#777777]">
+              Ülkemizde başta Afyon olmak üzere bir çok noktasından oniks mermer
+              taşının çıkarıldığını söyleyebilirim. Belirlenen ocaklardan
+              titizlikle çıkarılan ve kullanım için hazır hale getirilen onyx
+              mermer ürünleri çıkarıldıkları bölgeye has özellikleri üzerlerinde
+              taşırlar. Birbirinden farklı renk ve karakteristik özellik taşıyan
+              onyx mermer çeşitlerinin neler olduğunu şu şekilde sıralamamız
+              mümkün. Beyaz Oniks, Bal Oniks, Bubble Oniks, Cappuccino Oniks,
+              Velluto Oniks, Gümüş Oniks, Gold Oniks, Cocacola Oniks, Satin
+              Oniks, Salmon Oniks, Dark Desert Oniks ve Light Desert Oniks.
+              Yukarıda belirttiğimiz onyx mermer çeşitlerini Ülkemizin en
+              kaletile madenlerine sahip Afyon ilimizdeki maden ocaklarımızdan
+              çıkarıyor ve titizlikle sizler için hazırlıyoruz. Renk bilgileri
+              ve stok durumu için bizimle hemen iletişime geçebilir veya satış
+              mağazalarımızdan ürünlerimizi inceleyebilirsiniz.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Onyx Mermer Nereden Çıkıyor?
+            </h2>
+            <p className="text-[#777777]">
+              Doğal taş türleri arasında nadir çıkarılan ve bir o kadar değerli
+              olan onyx mermer ocakları ülkemizin bir çok bölgesinde bulunmak
+              ile birlikte en değerli maden ocakları Afyon bölgesinde
+              bulunmaktadır. Özellikle Afyon Emirdağ bölgesi bu alanda en
+              kaliteli oniks ocakları olarak kabul görmektedir. Firmamız bu
+              bölgede sahip olduğu maden ocaklarından sizler için onyx mermer
+              bloklarını titizlikte çıkarmakta ve işlemektedir. Çıkarıldığı
+              bölgeye has renklere sahip olan oniks mermer ürünleri doğal
+              olduğundan bloklar arasındaki renk geçişlerinde farklık
+              oluşabilmektedir. Aynı renk tonundaki bloklardaki ürünlerin
+              içindeki renk geçişlerinin farklılık göstermesi bu sebeple gayet
+              doğal karşılanmaktadır. Diğer Mermer veya Granit Mermer ürünlerine
+              göre çok daha az çıkarılan ve işlenen oniks mermer çeşitlerini
+              sizler için farklı kalınlıkta ve ebatlarda hazırlama imkanına
+              sahibiz. Bu sebeple kullanım alanınıza en uygun ürün bilgisi için
+              hemen bizimle iletişime geçebilirsiniz.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Onyx (Oniks) Mermer Fiyatları
+            </h2>
+            <p className="text-[#777777]">
+              Onyx mermer fiyatları diğer mermer fiyatlarına göre oldukça
+              değişken bir yapıya sahiptir. Bunun en büyük nedeni madenden
+              çıkarılan oniks mermerin yapısına veya görselliğine göre
+              fiyatlandırılmasıdır. Işık geçirgenlik oranı ve saydamlığı
+              fiyatının etkileyen en önemli özelliktir. Bununla birlikte ürünün
+              saydamlığı fiyatını doğrudan etkilemektedir. Ayrıca istenilen ebat
+              ve kalınlığı fiyatı etkileyen bir diğer husustur. Oniks mermer
+              blokları diğer mermer bloklarına göre çok daha zor çıkarıldığından
+              ve daha zor işlendiğinden dolayı piyasada az bulunmaktadır. Ürünün
+              stok durumu, temin edilebilme imkanı ve ebat bilgilerinin
+              belirlenmesinden sonra gerçek fiyat aralığını öğrenmeniz mümkün
+              olmaktadır.
+            </p>
           </div>
         </div>
       </div>
--- src/pages/dogal-taslar/DogalTaslarTraverten.jsx
+++ src/pages/dogal-taslar/DogalTaslarTraverten.jsx
@@ -57,7 +57,11 @@ const DogalTaslarTraverten = () => {
         </div>
 
         <div className="p-4 flex flex-col items-center flex-[4]">
-          <h1 className="text-center text-3xl font-bold my-4">Traverten</h1>
+          <p className="xl:pl-8 xl:m-4 py-4 pb-8 text-[#777777]">
+            Lüks ve estetiğin birleşimi olan Efesus traverten mermer taş
+            çeşitleri ve fiyatları için Efesus Stone yerli ve benzersiz
+            modelleri şimdi inceleyin!
+          </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center gap-4 ">
             {dogalTaslarTraverten[page].map((tas, index) => {
               return (
@@ -106,6 +110,181 @@ const DogalTaslarTraverten = () => {
                 onClick={nextHandler}
               />
             </button>
+          </div>
+          <div className="w-full flex flex-col gap-4 mt-4 border-t pt-2">
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Çeşitleri ve Fiyatları
+            </h2>
+            <p className="text-[#777777]">
+              Doğal taş çeşitleri arasında yapı malzemesi olarak sıklıkla
+              kullanılan traverten taşlar kalsiyum ve kobalt bileşiminden
+              meydana gelmektedir. Oluşumu milyonlarca yıl süren bu dayanıklı
+              yapı malzemesi geçmişten günümüze birçok farklı alanda yapı
+              malzemesi olarak kullanılmaya devam etmektedir. Doğal delikli
+              yapısı ve sağlam karakteristik özelliği nedeniyle insanoğlu yapı
+              malzemesi olarak bu ürüne büyük ilgi göstermektedir. Bu kapsamda
+              son dönemde sıklıkla merak edilen;{" "}
+              <span className="font-bold">
+                Traverten Çeşitleri ve Fiyatları
+              </span>
+              , Traverten taş kullanım alanları ve traverten kalitesi nasıl
+              anlaşılır sorularınıza cevap vermek istiyorum.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Nedir?
+            </h2>
+            <p className="text-[#777777]">
+              Kireç taşı familyasından olan traverten taşları kaplıca sularının
+              çökelmesi sonucunda meydana gelmektedir. Granit veya mermer
+              oluşumuna göre çok daha uzun bir zamanda bu taşların oluşumu
+              tamamlanmaktadır. Bu nedenle oldukça değerli ve karakteristik bir
+              yapıya sahiptir. Kendine özgü olan delikli yapısı bu ürüne çok
+              daha doğal bir görünüm kazandırmaktadır. Koyu kahverengiden bej
+              rengine kadar çeşitli renk kartelasına sahiptir. Bu kadar çok renk
+              seçeneğinin oluşması kullanım alanlarında daha fazla tercih edilme
+              nedeni olarak dikkat çekmektedir. Farklı renklerde oluşan damar
+              yapısı ile benzersiz bir özelliğe sahip olduğundan her bir
+              traverten taş karosunda farklı renk dalgalanması görmek mümkündür.
+              Bununla birlikte mermer ve granitlere oranlara çok daha yumuşak
+              bir yapıya sahip olan traverten taşlarının işlenmesi çok daha
+              kolaydır. Yoğunluğunun diğer doğal taşlara oranla düşük olması bu
+              ürünün çok daha hafif olmasını sağlamaktadır Hafif olduğundan
+              dolayı cephe kaplamalarında uygulandığı bölgeye daha az ağırlık
+              verdiğinden; güvenli ve sağlıklı bir kaplama ürünüdür.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Nerelerde Kullanılır?
+            </h2>
+            <p className="text-[#777777]">
+              Traverten taşlar doğal yapısı sayesinde hem iç mekanlarda hem de
+              dış mekanlarda rahatlıkla kullanılmaktadır. Serin yapısı ve
+              kaydırmaz özelliği bu ürünün ıslak zeminlerde dahil birçok alanda
+              kullanımına imkan tanımaktadır. Isıyı bünyesinde tutmaması
+              nedeniyle hem dış cephe kaplamalarında hem de havuz kenarı gibi
+              alanlarda rahat bir kullanım imkanı sağlar. Dış mekan Traverten
+              mermer kullanım alanları; Havuz kenarı gibi ıslak zeminlerde
+              Bahçelerin yürüyüş yollarında Veranda ve tüm dış mekan
+              zeminlerinde Dış mekan duvar kaplamalarında Barbeküler Havuz
+              basamakları Dış mekan merdiven basamakları Yukarıda belirttiğimiz
+              dış mekanların dışında kalan ve iç mekan olarak bilinen banyolarda
+              bu ürün rahatlıkla kullanılmaktadır. Kaymaz bir yapıya sahip
+              olması banyolarda sağlıklı bir kullanım imkanı sağlamaktadır.
+              Ayrıca iç mekan mutfak ve iç duvar kaplama malzemesi olarak da
+              tercih edebilirsiniz. Kendine has renk seçenekleri ve özel delikli
+              yapısı ile doğal bir görünüm elde etmek isteyenlerin ilk tercihi;
+              traverten mermer ürünleridir.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Dayanıklı Mıdır?
+            </h2>
+            <p className="text-[#777777]">
+              Bu doğal taş ürünleri granit ve mermer ürünleri kadar sert bir
+              yapıya sahip olmasa da sağlam ve uzun ömürlü bir üründür. Bunun en
+              büyük ispatı yüzyıllar öncesinde kullanılan yapıların günümüze
+              kadar ulaşmasıdır. Özellikle yer döşemesinde kullanılan bu ürün
+              yumuşak dokusu nedeniyle eskimeye karşı çok daha dayanıklıdır.
+              Doğal fiziksel etkenlere karşı son derece dirençli olan traverten
+              taşlarının bakımları doğru yapıldığı takdirde uzun yıllar
+              sorunsuzca kullanılabilir. Her doğal taşta olduğu gibi traverten
+              mermer kullanımında kimyasal ürünlerin kullanılmaması
+              gerekmektedir. Asit ve yoğun kimyasal ürünler traverten taş
+              ürünlerinin aşınmasına ve özelliğini kaybetmesine etki etmektedir.
+              Bu nedenle traverten ürünlerin temizliği yapılırken oldukça
+              dikkatli olunmalıdır.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Çeşitleri Nelerdir?
+            </h2>
+            <p className="text-[#777777]">
+              Traverten taş çeşitleri çıkarıldıkları bölgeye göre farklılık
+              göstermektedir. Genel olarak beyaz traverten ve bej traverten
+              taşları olarak bilenen ürünün renkleri sarı çakıl ve beyaz çakıl
+              olarak da adlandırılır. Kendine has özel delikle bir yapıya sahip
+              olan bu ürünler istenildiği zaman özel dolgularla bu delikler
+              kapatılabilmektedir. Bununla birlikte özel olarak hazırlanan bu
+              ürünlerin yüzeyleri;
+              <ul>
+                <li>* Cilalı yüzey Eskitmeli yüzey (Antik Görünüm)</li>
+                <li>* Honlu yüzey (Mat)</li>
+                <li>* Fırçalı yüzey (Patinato)</li>
+                <li>* Kenar kırma</li>
+              </ul>
+              Son dönemde bu ürünler arasında en çok ilgiyi eskitme olarak
+              bilinen; Antik Görünümlü traverten taşlar almaktadır.
+              Belirttiğimiz özelliklere sahip Antique Pewter, Gümüş traverten,
+              River traverten, Beyaz Traverten, Noce Traverten ve Klasik
+              Traverten çeşitlerimizle sizlere en kaliteli ürünleri sunuyoruz.
+              İhtiyaç duyduğunuz renk ve ebatlardaki traverten siparişleriniz
+              için bizimle hemen iletişime geçebilirsiniz.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Kalitesi Nasıl Anlaşılır?
+            </h2>
+            <p className="text-[#777777]">
+              Traverten Çeşitleri ve Fiyatları araştırması yapanlar için bu
+              ürünün özellikleri belirttikten sonra kaliteli bir traverten
+              taşının nasıl olması gerektiği hakkında bilgi vermek istiyorum.
+              Öncelikli olarak bilinmesi gereken kullanım alanına en uygun
+              traverten taşının tercih edilmesidir. Tercih etmek istediğiniz
+              traverten taşını iç mekan için mi dış mekan için mi kullanmak
+              istediğinizle başlayabiliriz. Bununla birlikte istenilen ebat ve
+              renk seçenekleri sizin hangi taşı tercih etmeniz gerektiğini
+              belirleyecektir. Traverten taşın kalitesini anlayabilmek için
+              öncelikli olarak yumuşak mı sert mi bir ürün talep ettiğiniz ile
+              alakalıdır. Yüzeyi pürüzsüz dekoratif bir traverten mermer
+              ihtiyacınız için daha yumuşak yapıdaki ürünler sizin için en ideal
+              ürün olacaktır. Aynı şekilde dış mekanların zeminlerinde
+              kullanılmak istenilen bu tür bir ürün için daha sert yapıdaki
+              ürünleri tercih etmeniz gerekmektedir. Bu konuda yapılacak en
+              basit yöntem ise; küçük bir parça traverten taşının kırılmasıyla
+              belirlenebilir. Traverteni kolaylıkla kırabilirseniz yumuşak
+              yapıda bir traverten taşı olduğunu anlayabilirsiniz.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Mermer Nerelerde Kullanılır?
+            </h2>
+            <p className="text-[#777777]">
+              Doğal taşlar arasında olan ve kullanım alanı bakımından oldukça
+              geniş bir yelpazeye sahip olan traverten taşları iç ve dış
+              mekanlar olmak üzere birçok yaşam alanında kullanılabilmektedir.
+              Özellikle doğal ve eşsiz bir görünüm elde etmek adına yaşam
+              alanlarının banyo, mutfak ve salonlarında bu tür ürünler
+              rahatlıkla kullanılmaktadır. Ayrıca granit mermer ürünlerine göre
+              çok daha hafif olmasından dolayı duvar dekorasyonlarında sıklıkla
+              tercih edilmektedir. İç mekan duvar dekorasyonunun yanında dış
+              duvarlarda yalıtım malzemesi olarak da tercih edilmektedir. Su
+              geçirmez ve serin yapısı traverten taşların dış duvarlarda tercih
+              edilme nedenidir. Ülkemizin farklı bölgelerindeki madenlerden elde
+              edilen bu ürünler; çıkarıldıkları bölgeye özgü renk seçenekleriyle
+              bizlere alternatifler sunmaktadır. Kullanılmak istenilen
+              mekanlardaki mobilyalara uygun renk bulmak su sebeple oldukça
+              kolaydır. Dünyanın en kaliteli maden ocaklarına sahip
+              olduğumuzdan; istediğiniz renk, ebat ve kalitedeki traverten taş
+              ürünlerine hemen ulaşmak adına bizimle iletişim kurmanız yeterli
+              olacaktır.
+            </p>
+            <h2 className="text-3xl font-bold text-[#52536e]">
+              Traverten Fiyatları 2023
+            </h2>
+            <p className="text-[#777777]">
+              Traverten Çeşitleri ve Fiyatları birden fazla etkene bağlı olarak
+              değişiklik göstermektedir. Bu sebeple traverten fiyatları 2023
+              yılında tercih edilecek renk, ebat ve boyutuna göre farklılık
+              göstermektedir. Bununla birlikte bu ürünün fiyatını belirleyen
+              kriterlerden bir diğerinin de kesim türü olduğunu belirtmek
+              isterim. Tercih edilen ürünün kullanım alanına göre özel olarak
+              belirlenen bu kesim türü ürün fiyatının belirleyecektir. Kısaca
+              özetlemek gerekirse traverten fiyatlarını etkileyen birden fazla
+              etken bulunmaktadır. Bu etkenleri birlikte değerlendirmek ve
+              sizler için en ideal traverten türünü belirlemek adına sizleri
+              satış mağazamıza bekliyoruz. Yaşam alanlarınıza doğal bir güzellik
+              ve görünüm kazandırmak adına uygulamayı düşündüğünüz traverten
+              mermer ürünleri hakkında tüm merak ettikleriniz için bizimle
+              iletişime geçebilirsiniz. Talep ettiğiniz ürünün rengi, türü,
+              boyutu ve planlanan uygulama alanın diğer ihtiyaçlarına göre;
+              Ülkemizin en kaliteli madenlerinden çıkarılmış özel traverten taş
+              ürünleri ve fiyatlarımızla yanınızdayız.
+            </p>
           </div>
         </div>
       </div>
