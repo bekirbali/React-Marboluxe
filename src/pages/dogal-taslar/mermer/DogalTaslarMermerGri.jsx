0ed48eaedb7ebb9b81415baa14f37c08d325c452
bekirbali <bfbali43@gmail.com>
2023-11-13 22:07:56 +0300

    Refactor code and update UI

--- src/pages/dogal-taslar/mermer/DogalTaslarMermerGri.jsx
+++ src/pages/dogal-taslar/mermer/DogalTaslarMermerGri.jsx
@@ -15,7 +15,20 @@ const DogalTaslarMermerGri = () => {
         </div>
 
         <div className="p-4 flex flex-col items-center flex-[4]">
-          <h1 className="text-center text-3xl font-bold my-4">Gri Mermer</h1>
+          <h1 className="font-bold text-3xl text-[#52536e]">
+            Gri Mermer Çeşitleri ve Modelleri
+          </h1>
+          <p className="xl:pl-8 xl:m-4 py-4 pb-8 text-[#777777]">
+            <span className="font-bold">Gri mermer</span>, evinize doğal
+            güzellik katabilen, görsel olarak hoş bir doğal taştır. Sade ama
+            sofistike gri mermer, hemen hemen her dekorla iyi uyum sağlar.
+            Aradığınız ister açık gri mermer, ister koyu gri mermer olsun,
+            zevkinize uygun sayısız seçenek var. Gri mermer tezgahlar,
+            backsplash’ler, makyaj tezgahları veya diğer ev yapıları, herhangi
+            bir eve harika bir ektir. Fazla gösterişli olmayan şık bir mermer
+            çeşidi istiyorsanız gri mermer sizin için mükemmel bir seçenek
+            olabilir.
+          </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center gap-4 ">
             {dogalTaslarMermerGri[0].map((tas, index) => {
               return (
@@ -39,6 +52,21 @@ const DogalTaslarMermerGri = () => {
                 </div>
               );
             })}
+          </div>
+          <div className="w-full flex flex-col gap-4 mt-4 border-t pt-2">
+            <p className="text-[#777777]">
+              <span className="font-bold">Gri mermer</span>, açıktan koyuya
+              kadar bir dizi gri tonuna sahip bir doğal taş türüdür. Şık ve
+              modern görünümü nedeniyle oldukça değerlidir ve genellikle çağdaş
+              iç ve dış tasarım projelerinde kullanılır. Gri mermer,
+              dayanıklılığı ve çizilmeye karşı direnci ile bilinir, bu da onu
+              döşeme ve tezgah gibi yüksek trafikli alanlar için popüler bir
+              seçim haline getirir. Çok yönlülüğü ve zarif güzelliği, onu
+              minimalistten klasiğe kadar çeşitli tasarım stilleri için popüler
+              bir seçim haline getiriyor. Gri mermer, herhangi bir alana
+              sofistike ve lüks bir dokunuş katarak onu mimarlar, tasarımcılar
+              ve ev sahipleri için popüler bir seçim haline getirir.
+            </p>
           </div>
         </div>
       </div>
--- src/pages/dogal-taslar/mermer/DogalTaslarMermerYesil.jsx
+++ src/pages/dogal-taslar/mermer/DogalTaslarMermerYesil.jsx
@@ -15,7 +15,18 @@ const DogalTaslarMermerYesil = () => {
         </div>
 
         <div className="p-4 flex flex-col items-center flex-[4]">
-          <h1 className="text-center text-3xl font-bold my-4">Yeşil Mermer</h1>
+          <h1 className="font-bold text-3xl text-[#52536e]">
+            Yeşil Mermer Çeşitleri ve Modelleri
+          </h1>
+          <p className="xl:pl-8 xl:m-4 py-4 pb-8 text-[#777777]">
+            <span className="font-bold">Yeşil mermer</span>, yerleştirildiği her
+            alana renk ve yaşam katacak harika bir doğal taştır. Mermerinizle
+            bir açıklama yapmak istiyorsanız, yeşil mermer tezgahlar,
+            backsplash’ler, makyaj tezgahları veya diğer ev yapıları tam da bunu
+            yapacaktır. Ek olarak, çekiciliğini daha da artırabilecek güzel zıt
+            damarlara sahip bir taş bulabilirsiniz. Yeşil mermer, hem güzel hem
+            de cesur bir taş eklemek için harika bir seçimdir.
+          </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center gap-4 ">
             {dogalTaslarMermerYesil[0].map((tas, index) => {
               return (
@@ -39,6 +50,19 @@ const DogalTaslarMermerYesil = () => {
                 </div>
               );
             })}
+          </div>
+          <div className="w-full flex flex-col gap-4 mt-4 border-t pt-2">
+            <p className="text-[#777777]">
+              <span className="font-bold">Yeşil mermer</span>, çarpıcı yeşil
+              rengi ve benzersiz damar desenleriyle oldukça değer verilen bir
+              doğal taş türüdür. Döşeme, duvar kaplama, tezgah, iç ve dış
+              tasarım uygulamalarında yaygın olarak kullanılır. Yeşil mermer,
+              dayanıklılığı ve aşınmaya ve yıpranmaya karşı direnci ile bilinir
+              ve bu da onu yüksek trafikli alanlar için popüler bir seçim haline
+              getirir. Doğal güzelliği ve zarafeti, herhangi bir alana lüks ve
+              sofistike bir dokunuş katarak onu mimarlar, tasarımcılar ve benzer
+              şekilde ev sahipleri için popüler bir seçim haline getirir.
+            </p>
           </div>
         </div>
       </div>
