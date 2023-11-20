e9e73981a7cc245d0c430e8ecd6a8ab597a681e9
bekirbali <bfbali43@gmail.com>
2023-11-20 23:02:12 +0300

    index

--- src/utils/holder.js
+++ src/utils/holder.js
@@ -5,10 +5,77 @@ export const afyonOcak = [
   { image: "/images/ocaklar/AfyonBeyazMermerOcak/ocak10-4-600x400.avif" },
   { image: "/images/ocaklar/AfyonBeyazMermerOcak/Afyon1-711x400.avif" },
 ];
-export const marmaraOcak = [];
-export const gümüsOcak = [];
-export const beyazOcak = [];
-export const brecciaOcak = [];
-export const kirmiziOcak = [];
-export const burdurOcak = [];
-export const klasikOcak = [];
+
+export const marmaraOcak = [
+  {
+    image:
+      "/images/ocaklar/MarmaraBeyazMermerOcak/WhatsApp-Image-2022-09-13-at-17.53.18-300x400.avif",
+  },
+  {
+    image:
+      "/images/ocaklar/MarmaraBeyazMermerOcak/1-e1663565562144-754x400.avif",
+  },
+  { image: "/images/ocaklar/MarmaraBeyazMermerOcak/2-528x400.avif" },
+  {
+    image:
+      "/images/ocaklar/MarmaraBeyazMermerOcak/3-e1663565484565-468x400.avif",
+  },
+  { image: "/images/ocaklar/MarmaraBeyazMermerOcak/6-2-711x400.avif" },
+  { image: "/images/ocaklar/MarmaraBeyazMermerOcak/11-303x400.avif" },
+];
+
+export const gümüsOcak = [
+  { image: "/images/ocaklar/GumusTravertenOcak/1-3-605x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/2-3-587x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/3-5-681x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/5-5-605x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/6-3-562x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/7-3-533x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/9-3-594x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/10-3-586x400.avif" },
+  { image: "/images/ocaklar/GumusTravertenOcak/11-6-606x400.avif" },
+];
+
+export const beyazOcak = [
+  { image: "/images/ocaklar/BeyazTravertenOcak/1-6-600x400.avif" },
+  { image: "/images/ocaklar/BeyazTravertenOcak/2-6-602x400.avif" },
+  { image: "/images/ocaklar/BeyazTravertenOcak/3-8-583x400.avif" },
+  { image: "/images/ocaklar/BeyazTravertenOcak/4-6-602x400.avif" },
+  { image: "/images/ocaklar/BeyazTravertenOcak/5-8-602x400.avif" },
+];
+
+export const brecciaOcak = [
+  { image: "/images/ocaklar/BrecciaBardiglioOcak/1-ocak3-1-533x400.avif" },
+  { image: "/images/ocaklar/BrecciaBardiglioOcak/2-ocak4-1-533x400.avif" },
+  { image: "/images/ocaklar/BrecciaBardiglioOcak/3-ocak1-1-533x400.avif" },
+  { image: "/images/ocaklar/BrecciaBardiglioOcak/4-ocak7-1-533x400.avif" },
+  { image: "/images/ocaklar/BrecciaBardiglioOcak/5-ocak2-1-533x400.avif" },
+];
+
+export const kirmiziOcak = [
+  { image: "/images/ocaklar/KirmiziTravertenOcak/1-ocak5-602x400.avif" },
+  { image: "/images/ocaklar/KirmiziTravertenOcak/2-ocak7-602x400.avif" },
+  { image: "/images/ocaklar/KirmiziTravertenOcak/3-ocak9-602x400.avif" },
+  { image: "/images/ocaklar/KirmiziTravertenOcak/4-ocak8-602x400.avif" },
+  { image: "/images/ocaklar/KirmiziTravertenOcak/5-ocak3-602x400.avif" },
+];
+
+export const burdurOcak = [
+  { image: "/images/ocaklar/BurdurBejOcak/1-ocak2-1400x787.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/2-ocak11-1205x800.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/3-ocak12-1400x761.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/4-ocak13-1267x800.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/5-ocak14-1400x651.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/6-ocak3-1400x787.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/7-ocak1-1400x787.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/8-ocak4-1400x787.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/9-ocak10.avif" },
+  { image: "/images/ocaklar/BurdurBejOcak/10-ocak15-1262x800.avif" },
+];
+
+export const klasikOcak = [
+  { image: "/images/ocaklar/KlasikNoceTravertenOcak/1-ocak1-712x400.avif" },
+  { image: "/images/ocaklar/KlasikNoceTravertenOcak/2-ocak3-534x400.avif" },
+  { image: "/images/ocaklar/KlasikNoceTravertenOcak/3-ocak5-534x400.avif" },
+  { image: "/images/ocaklar/KlasikNoceTravertenOcak/4-ocak4-534x400.avif" },
+];
