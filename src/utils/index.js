10147fdacb7d49b4598a6ebfb66e4e1bf1f710c6
bekirbali <bfbali43@gmail.com>
2023-12-15 23:28:36 +0300

    Add project references to utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -47555,3 +47555,170 @@ export const klasikOcak = [
   { image: "/images/ocaklar/KlasikNoceTravertenOcak/3-ocak5-534x400.avif" },
   { image: "/images/ocaklar/KlasikNoceTravertenOcak/4-ocak4-534x400.avif" },
 ];
+
+export const projects = [
+  {
+    image: "/images/projectCovers/ciraganKapak.jpg",
+    name: "CIRAGAN PALACE RESTORATION PROJECT",
+  },
+  {
+    image: "/images/projectCovers/peraPalaceKapak.jpeg",
+    name: "PERA PALACE HOTEL",
+  },
+  {
+    image: "/images/projectCovers/bulgariaKapak.jpeg",
+    name: "PORTFOLIO STUDIES",
+  },
+  {
+    image: "/images/projectCovers/bookMatchKapak.png",
+    name: "BOOKMATCH",
+  },
+  {
+    image: "/images/projectCovers/sagalassosKapak.png",
+    name: "SAGALASSOS ANCIENT CITY RESTORATION",
+  },
+  {
+    image: "/images/projectCovers/poolCapingKapak.png",
+    name: "POOL COPING",
+  },
+  {
+    image: "/images/projectCovers/mozartKapak.png",
+    name: "MOZART BENGHAZI – LIBYA",
+  },
+  {
+    image: "/images/projectCovers/turkishBathroomKapak.jpg",
+    name: "TURKISH BATH REFERENCES",
+  },
+  {
+    image: "/images/projectCovers/CNCKapak.jpg",
+    name: "CNC WALL DECORATION REFERENCES",
+  },
+  {
+    image: "/images/projectCovers/stairsKapak.jpg",
+    name: "STAIRS REFERENCES",
+  },
+  {
+    image: "/images/projectCovers/vivaSuitKapak.png",
+    name: "VIVA SUIT CAPPADOCIA",
+  },
+  {
+    image: "/images/projectCovers/silverOnixKapak.png",
+    name: "SILVER ONYX BULGARIA PROJECT",
+  },
+  {
+    image: "/images/projectCovers/waterjetKapak.png",
+    name: "Waterjet References",
+  },
+  {
+    image: "/images/projectCovers/suraKapak.jpg",
+    name: "SURA HOTEL",
+  },
+  {
+    image: "/images/projectCovers/bestWesternKapak.jpg",
+    name: "BEST WESTERN PREMIER CAPPADOCIA",
+  },
+  {
+    image: "/images/projectCovers/sarnıçKapak.jpg",
+    name: "SARNIC RESTAURANT – ISTANBUL",
+  },
+  {
+    image: "/images/projectCovers/SuadiyetheHouseCafeKapak.jpg",
+    name: "THE HOUSE CAFE – SUADIYE",
+  },
+  {
+    image: "/images/projectCovers/bulgariaKapak.jpeg",
+    name: "BULGARIA PROJECT",
+  },
+  {
+    image: "/images/projectCovers/tekirovaKapak.jpg",
+    name: "TEKIROVA QUEEN’S PARK",
+  },
+  {
+    image: "/images/projectCovers/benerKapak.jpg",
+    name: "BENER LAW OFFICE",
+  },
+  {
+    image: "/images/projectCovers/queenParkKapak.jpg",
+    name: "QUEEN’ S PARK HOTEL",
+  },
+  {
+    image: "/images/projectCovers/baltaLimanıKapak.jpg",
+    name: "BALTALIMANI",
+  },
+  {
+    image: "/images/projectCovers/lagünKapak.jpg",
+    name: "LAGUN IRIS",
+  },
+  {
+    image: "/images/projectCovers/kanyonKapak.jpg",
+    name: "KANYON AVM – ESCALE",
+  },
+  {
+    image: "/images/projectCovers/arkadiaKapak.jpg",
+    name: "ARKADIA REZIDANS",
+  },
+  {
+    image: "/images/projectCovers/afyonKapak.jpg",
+    name: "AFYON GOVERNOR RESTORATION",
+  },
+  {
+    image: "/images/projectCovers/theHouseKapak.jpg",
+    name: "THE HOUSE CAFE",
+  },
+  {
+    image: "/images/projectCovers/qatarKapak.jpg",
+    name: "QATAR PROJECT",
+  },
+  {
+    image: "/images/projectCovers/poolCapingKapak.png",
+    name: "TRAVERTINE REFERENCES",
+  },
+  {
+    image: "/images/projectCovers/longbeachKapak.jpg",
+    name: "LONG BEACH RESORT HOTEL",
+  },
+  {
+    image: "/images/projectCovers/historicalKapak.jpg",
+    name: "HISTORICAL REISOĞLU MANSION AND SELCUK’S TURKISH BATH",
+  },
+  {
+    image: "/images/projectCovers/zuleyhaKuruKapak.jpg",
+    name: "ZULEYHA KURU FASHION HOUSE",
+  },
+  {
+    image: "/images/projectCovers/bodrumVillaKapak.jpg",
+    name: "BODRUM VILLA PROJECT",
+  },
+  {
+    image: "/images/projectCovers/silverUkraineKapak.jpg",
+    name: "SILVER ONYX UKRAINE PROJECT",
+  },
+  {
+    image: "/images/projectCovers/taskiranKapak.jpg",
+    name: "TASKIRAN MEDICAL AESTHETICS CLINIC",
+  },
+  {
+    image: "/images/projectCovers/UKRestorationKapak.jpg",
+    name: "UK RESTORATION PROJECT",
+  },
+  {
+    image: "/images/projectCovers/silverOnixTravertainKapak.jpeg",
+    name: "SILVER ONYX – SILVER TRAVERTINE PROJECT",
+  },
+  {
+    image: "/images/projectCovers/abuHayatKapak.jpg",
+    name: "ABU HAYAT CAVE HOTEL",
+  },
+  {
+    image: "/images/projectCovers/adoraHotelKapak.jpg",
+    name: "ADORA HOTEL & RESORT PROJECT",
+  },
+  {
+    image: "/images/projectCovers/rividyaKapak.png",
+    name: "RIVIDA VILLA PROJECT",
+  },
+  {
+    image: "/images/projectCovers/halicProjectKapak.jpeg",
+    name: "HALIC PROJECT",
+  },
+];
