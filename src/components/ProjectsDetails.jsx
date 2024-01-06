af4ba21ba5c4d1e826db7b3502f8ce9b4978daa7
bekirbali <bfbali43@gmail.com>
2024-01-07 01:34:39 +0300

    projects context

--- src/components/ProjectsDetails.jsx
+++ src/components/ProjectsDetails.jsx
@@ -9,61 +9,40 @@ import {
   plakalar,
   ozelKoleksiyon,
 } from "../utils";
+import { useTranslation } from "react-i18next";
 
 const ProjectsDetails = () => {
   const location = useLocation();
   const { state } = location;
 
-  const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const [modal, setModal] = useState(false);
-  const [currentArray, setCurrentArray] = useState("");
-
-  const scrollRef = useRef();
-
-  const scrollHandler = () => {
-    console.log(scrollRef.current.scrollTop);
-    console.log(modal);
-  };
-
-  useEffect(() => {
-    setCurrentArray(state.array);
-    console.log(state.context);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
-    <div
-      ref={scrollRef}
-      onScroll={scrollHandler}
-      onClick={() => console.log(scrollRef.current.scrollTop)}
-      style={{ height: "100vh", overflow: "hidden" }}
-      className="relative overflow-hidden"
-    >
-      <div className="h-[120px] w-full flex justify-center items-center bg-slate-700 text-white">
-        <h1 className="font-bold text-3xl">PROJELER</h1>
+    <>
+      <div className="h-[120px] flex justify-center items-center bg-slate-700 text-white">
+        <h1 className="font-bold text-3xl">{t("PROJELER")}</h1>
       </div>
-      <div className="flex justify-around h-[500px] p-4 w-[90%] mx-auto">
-        <div onClick={() => setModal(true)} className="flex gap-4">
-          {state.context.map((test) => {
-            return (
-              <>
-                <img src={test} alt="" className="w-[600px]" />
-              </>
-            );
-          })}
+      <div className="min-h-[70vh] flex flex-wrap justify-center items-center gap-2 overflow-hidden">
+        {state.context.map((photo, index) => {
+          return (
+            <div
+              key={index}
+              onClick={() => setModal(true)}
+              className="h-[500px] w-[75%] md:w-[49%] lg:w-[24%]"
+            >
+              <img src={photo} alt="" className="w-full h-full" />
+            </div>
+          );
+        })}
+      </div>
+      {modal && (
+        <div className="absolute w-[100vw] h-[100vh] top-0 ">
+          <DetailModal image={state.image} modal={modal} setModal={setModal} />
         </div>
-        {modal && (
-          <div className="absolute w-[100vw] h-[100vh] top-0 ">
-            <DetailModal
-              image={state.image}
-              modal={modal}
-              setModal={setModal}
-            />
-          </div>
-        )}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
